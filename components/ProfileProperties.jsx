ca16756fb47c7802cf77a97a6bd5ac7db90fb3b8
Alyssa Voccia <avoccia@hotmail.com>
2024-09-22 09:34:02 -0400

    Added action to delete property

--- components/ProfileProperties.jsx
+++ components/ProfileProperties.jsx
@@ -2,9 +2,20 @@
 import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import deleteProperty from '@/app/actions/deleteProperty';
 
 const ProfileProperties = ({ properties: initialProperties  }) => {
   const [properties, setProperties] = useState(initialProperties);
+
+  const handleDeleteProperty = async (propertyId) => {
+    const confirmed = window.confirm('Are you sure you want to delete this property?');
+
+    if (!confirmed) return;
+
+    await deleteProperty(propertyId);
+    const updatedProperties = properties.filter((property) => property._id !== propertyId);
+    setProperties(updatedProperties);
+  };
 
   return (
     properties.map((property) => (
@@ -20,7 +31,10 @@ const ProfileProperties = ({ properties: initialProperties  }) => {
           <a href="/add-property.html" className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600">
             Edit
           </a>
-          <button className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"type="button">
+          <button 
+            className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600" type="button"
+            onClick={() => handleDeleteProperty(property._id)}
+          >
             Delete
           </button>
         </div>
