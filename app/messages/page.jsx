e2636c2570093ace00955dc459202b3920a26af6
Alyssa Voccia <avoccia@hotmail.com>
2024-10-25 08:56:11 -0400

    Added mark as read action and functionality

--- app/messages/page.jsx
+++ app/messages/page.jsx
@@ -3,6 +3,7 @@ import Message from "@/models/Message";
 import '@/models/Property';
 import { convertToSerializedObject } from "@/utils/convertToObject";
 import { getSessionUser } from "@/utils/getSessionUser";
+import MessageCard from "@/components/MessageCard";
 
 const MessagesPage = async () => {
   connectDB();
@@ -37,7 +38,7 @@ const MessagesPage = async () => {
             <div className="space-y-4">
               {messages.length === 0 ? (<p>You have no messages.</p>) : (
                 messages.map((message) => (
-                  <h3 key={message._id}>{ message.name }</h3>
+                  <MessageCard key={message._id} message={message} />
                 ))
               )}
             </div>
---
+++ components/MessageCard.jsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import markMessageAsRead from '@/app/actions/markMessageAsRead';
+
+const MessageCard = ({ message }) => {
+  const [isRead, setIsRead] = useState(message.read);
+
+  const handleReadClick = async () => {
+    try {
+      const read = await markMessageAsRead(message._id);
+      setIsRead(read);
+      toast.success(`Message marked as ${read ? 'read' : 'unread'}`);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  return (
+    <div className="relative bg-white p-4 rounded-md shadow-md border border-gray-200">
+      {!isRead && (
+        <div className="absolute top-2 right-2 bg-yellow-500 text-white px-2 py-1 rounded-md text-sm font-bold">
+          New
+        </div>
+      )}
+      <h2 className="text-xl mb-4">
+        <span className="font-bold">Property Inquiry: </span>
+        { message.property.name }
+      </h2>
+      <p className="text-gray-700">{ message.body }</p>
+      <ul className="mt-4">
+        <li>
+          <strong>Reply Email: </strong>
+          <a className="text-blue-500" href={`mailto:${message.email}`}>{ message.email }</a>
+        </li>
+        <li>
+          <strong>Reply Phone: </strong>
+          <a className="text-blue-500" href={`tel:${message.phone}`}>{ message.phone }</a>
+        </li>
+        <li>
+          <strong>Received: </strong>
+          { new Date(message.createdAt).toLocaleString() }
+        </li>
+      </ul>
+      <button onClick={handleReadClick} className="mt-4 mr-3 bg-blue-500 text-white py-1 px-3 rounded-md">{ isRead ? 'Mark as Unread' : 'Mark as Read' }</button>
+      <button className="mt-4 bg-red-500 text-white py-1 px-3 rounded-md">Delete</button>
+    </div>
+  )
+};
+
+export default MessageCard;
