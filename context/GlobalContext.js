182a0c00b81cbd7dfade82af191103abb089b0ab
Alyssa Voccia <avoccia@hotmail.com>
2024-10-28 09:08:54 -0400

    Set up unread message change when marking as read/unread and delete

--- context/GlobalContext.js
+++ context/GlobalContext.js
@@ -1,6 +1,8 @@
 'use client';
 
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import getUnreadMessageCount from '@/app/actions/getUnreadMessageCount';
 
 // Create Context
 const GlobalContext = createContext();
@@ -8,6 +10,16 @@ const GlobalContext = createContext();
 // Create a provider for the GlobalContext
 export function GlobalProvider({ children }) {
   const [unreadCount, setUnreadCount] = useState(0);
+
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session && session.user) {
+      getUnreadMessageCount().then((res) => {
+        if (res.count) setUnreadCount(res.count);
+      });
+    }
+  }, [getUnreadMessageCount, session]);
 
   return (
     <GlobalContext.Provider value={{ unreadCount, setUnreadCount }}>
